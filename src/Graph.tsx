080b076d034c8bd53be4e44705d69be446ed1354
8man <61812897+NayaabC@users.noreply.github.com>
2024-06-17 13:07:00 -0400

    Task 2 - Change visual to dynamic line-graph

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement{
   load: (table: Table) => void,
 }
 
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -49,6 +49,16 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute('view', 'y_line');                    // Change graph visual to a line graph
+      elem.setAttribute('column-pivots', '["stock"]');        // Distinguish from different stocks
+      elem.setAttribute('row-pivots', '["timestamp"]');       // Map datapoint to timestamps
+      elem.setAttribute('columns', '["top_ask_price"]');      // Specify which data we want to plot
+      // Aggregates handles duplicated data -> consolidates into a single point by taking average
+      elem.setAttribute('aggregates', `
+        {"stock":"distinct count",
+        "top_ask_price":"avg",
+        "top_bid_price":"avg",
+        "timestamp":"distinct count"}`);
     }
   }
 
